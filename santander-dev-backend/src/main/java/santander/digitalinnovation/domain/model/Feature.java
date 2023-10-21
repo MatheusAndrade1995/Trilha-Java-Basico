08aa91d9b2c2566d7703a521f87ad4c352d8e0cf
MatheusAndrade1995 <114603315+MatheusAndrade1995@users.noreply.github.com>
2023-10-21 13:51:10 -0300

    Delete santander-dev-backend directory
    Delete santander-dev-backend directory

--- santander-dev-backend/src/main/java/santander/digitalinnovation/domain/model/Feature.java
+++
@@ -1,8 +0,0 @@
-package santander.digitalinnovation.domain.model;
-
-import jakarta.persistence.*;
-
-@Entity(name = "tb_feature")
-public class Feature extends BaseItem {
-	
-}
--- santander-dev-backend/src/main/java/santander/digitalinnovation/domain/model/News.java
+++
@@ -1,8 +0,0 @@
-package santander.digitalinnovation.domain.model;
-
-import jakarta.persistence.*;
-
-@Entity(name = "tb_news")
-public class News extends BaseItem {
-	
-}
